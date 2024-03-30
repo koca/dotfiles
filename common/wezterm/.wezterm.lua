2b172d6dca35a1b3e691ee2750198ed45def9dfb
Mesut Koca <mesut@TUIQ0M641K39J.local>
2024-03-30 23:29:26 +0300

    feat: init fonts and wezterm settings

---
+++ common/wezterm/.wezterm.lua
@@ -0,0 +1,31 @@
+local wezterm = require("wezterm")
+
+return {
+  font = wezterm.font_with_fallback({
+    {
+      family="FiraCode Nerd Font",
+      weight="Medium"
+    }
+  }),
+  font_size = 20,
+  color_scheme = "Catppuccin Mocha",
+  -- color_scheme = "Catppuccin Latte",
+  window_decorations = "RESIZE",
+  -- https://wezfurlong.org/wezterm/config/lua/config/window_padding.html
+  window_padding = {
+    left = 10,
+    right = 10,
+    top = 0,
+    bottom = 0,
+  },
+  keys = {
+    {
+      key = 'n',
+      mods = 'SHIFT|CTRL',
+      action = wezterm.action.ToggleFullScreen,
+    },
+  },
+}
+
+
+
